2148b4bd03b70c899abca324428158ee42c02b04
TanishqSingla <tanishqsingla00@gmail.com>
2022-10-21 18:10:14 +0530

    fix: port

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.post("/update", (req, res) => {
 	res.status(200).json(response);
 });
 
-app.listen(5000, () => console.log("Server listening on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server listening on port 5000"));
